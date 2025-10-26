b6cde3f027ae57582141d8d083032b1cf8ec2097
dionromero <dionalcantara20@gmail.com>
2025-10-26 16:11:46 -0300

    feat: adicionar importações e carregar dataset de roupas no módulo de modelagem

--- wardobre_ipynb/PROJETO/BACKEND/modelagem.py
+++ wardobre_ipynb/PROJETO/BACKEND/modelagem.py
@@ -1,2 +1,15 @@
-import clima
+import clima 
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from datasets import load_dataset
+
+
+# Carregar o dataset de roupas
+dataset = load_dataset("samokosik/clothes_simplified")
+df_train = dataset['train']
+df_test = dataset['test']
+
+# Função para preparar DataLoader
