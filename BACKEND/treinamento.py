06640bac61d7636c56286ccda9119b5c5b73e25c
dionromero <dionalcantara20@gmail.com>
2025-11-18 00:33:10 -0300

    refactor: remover importação de JSON não utilizada do arquivo treinamento.py

--- BACKEND/treinamento.py
+++ BACKEND/treinamento.py
@@ -3,7 +3,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
-import json
 from dados import preparar_dados_combinados
 from datasets import load_dataset
 from imagem import imagem_para_json  
