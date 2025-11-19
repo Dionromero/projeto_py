440c199ada9d4fcc6b52713b1236a1984727faea
dionromero <dionalcantara20@gmail.com>
2025-11-18 21:37:04 -0300

    feat(datasets): adicionar pré-processamento de dados para o conjunto de roupas
    Adicionar imports de pandas e numpy para manipulação de dados
    
    Implementar normalização dos rótulos para começarem em 0
    
    Converter o conjunto de dados para um pandas DataFrame para facilitar a análise

--- DATASETS/clothes.py
+++ DATASETS/clothes.py
@@ -1,5 +1,7 @@
-
-from datasets import load_dataset       
+import pandas as pd
+import numpy as np
+from datasets import load_dataset     
+  
 # Carrega o dataset 'clothes_simplified'
 dataset = load_dataset("samokosik/clothes_simplified")
 # Exibe informações do dataset
@@ -10,3 +12,32 @@ df_test = dataset['test']
 # Exemplo de acesso a um item
 print(df_train[0])
 
+df_train = dataset["train"].to_pandas()
+df_test = dataset["test"].to_pandas()
+
+print(df_train.head())
+
+categorias_numericas = df_train["label"].unique()
+print("Categorias numéricas:", categorias_numericas)
+
+tabela_categorias = (
+    df_train[["label"]]
+    .drop_duplicates()
+    .sort_values("label")
+)
+
+pd.set_option('display.max_rows', None)
+print(tabela_categorias)
+
+# normalizando os rotulos para começar de 0
+classes = np.unique(dataset['train']['label'])
+classes
+
+label_map = {old: new for new, old in enumerate(classes)}
+label_map
+
+def normalize_labels(example):
+    example['label'] = label_map[example['label']]
+    return example
+
+dataset = dataset.map(normalize_labels)
